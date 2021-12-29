3d03b23f2de34c7753c937792697c4d0e76d5113
Abderrahmane Ahmam <abderrahmaneahmam1@gmail.com>
2021-12-29 03:17:36 +0100

    Implement IMaterialsManager && and make AddUser more flexible

--- src/controls/CustomDialog.java
+++ src/controls/CustomDialog.java
@@ -6,9 +6,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class CustomDialog extends JDialog {
-    public CustomDialog(JFrame source, String title, Component cmp) {
+    public CustomDialog(JFrame source, String title, Component cmp,int width,int height) {
         super(source, title);
-        this.setSize(500,450);
+        this.setSize(width,height);
+        this.setMinimumSize(new Dimension(width,height));
         this.setLocationRelativeTo(null);
         this.add(cmp);
         this.setVisible(true);
--- src/managers/MaterialsManager.java
+++ src/managers/MaterialsManager.java
@@ -2,8 +2,6 @@ package managers;
 
 import common.interfaces.IMaterialsManager;
 import models.MaterialModel;
-import models.UserModel;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,13 +12,32 @@ class MaterialsManager extends Manager implements IMaterialsManager {
     }
 
     @Override
-    public ArrayList<MaterialModel> getMaterials() {
-        return null;
+    public ArrayList<MaterialModel> getMaterials() throws SQLException {
+        var result = db.getStatement().executeQuery("select id,name,picture from materials");
+        var materials = new ArrayList<MaterialModel>();
+        while(result.next()){
+            materials.add(new MaterialModel(result.getInt(1)
+                    ,result.getString(2),
+                    result.getString(3)));
+        }
+        return materials;
     }
 
     @Override
-    public ArrayList<MaterialModel> getMaterialsByName(String name) {
-        return null;
+    public ArrayList<MaterialModel> getMaterialsByName(String name) throws SQLException {
+        try {
+            var prs = db.getConnection().prepareStatement("select id,name,picture from materials where name like ? ");
+            prs.setString(1,"%"+name+"%");
+            var rs = prs.executeQuery();
+            var materials = new ArrayList<MaterialModel>();
+            while(rs.next()){
+                materials.add(new MaterialModel(rs.getInt(1),rs.getString(2), rs.getString(3)));
+            }
+            return materials;
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
     }
 
     @Override
@@ -50,17 +67,44 @@ class MaterialsManager extends Manager implements IMaterialsManager {
     }
 
     @Override
-    public boolean addMaterial(MaterialModel material) {
-        return false;
+    public boolean addMaterial(MaterialModel material){
+        try {
+            var prs = db.getConnection().prepareStatement("insert into materials (name,picture) values (?,?)");
+            prs.setString(1, material.getName());
+            prs.setString(2, material.getPicture());
+            int row = prs.executeUpdate();
+            return row != 0;
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 
     @Override
     public boolean updateMaterial(MaterialModel material) {
-        return false;
+        try {
+            var prs = db.getConnection().prepareStatement("update materials set name=?,picture=? where id=?");
+            prs.setString(1, material.getName());
+            prs.setString(2, material.getPicture());
+            prs.setInt(3, material.getId());
+            int row = prs.executeUpdate();
+            return row != 0;
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 
     @Override
     public boolean deleteMaterial(MaterialModel material) {
-        return false;
+        try {
+            var prs = db.getConnection().prepareStatement("delete from materials where id=?");
+            prs.setInt(1, material.getId());
+            int row = prs.executeUpdate();
+            return row != 0;
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 }
